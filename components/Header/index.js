24433a91a885ba6c085444072cd6b844cf418cec
Sam Decker <samdecker386@gmail.com>
2020-02-14 09:31:13 -0700

    Completed header componenet

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,32 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+// create a function that will create a header component that matches HTML
+function Header() {
+
+    // define new elements
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    // setup the structure of the elements
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+
+    // add classes to elements
+    header.classList.add('header');  
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    // add text content
+    date.textContent = 'March 28, 2019';   
+    title.textContent = 'Lambda Times';   
+    temp.textContent = '98°';    
+
+    return header;
+};
+
+const header = document.querySelector('.header-container');
+header.appendChild(Header());
